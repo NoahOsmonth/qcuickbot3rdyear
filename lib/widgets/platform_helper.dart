6dee827635d00499c16ffbc779e974d7bd087518
noahosmonth <sigmundsarino@gmail.com>
2025-04-23 22:11:22 +0800

    Update generated plugin registrant files for consistency

--- lib/widgets/platform_helper.dart
+++ lib/widgets/platform_helper.dart
@@ -1,4 +1,4 @@
 // This file conditionally exports the correct PlatformHelper implementation.
 
 export '../utils/_platform_helper_stub.dart' // Stub implementation (web)
-    if (dart.library.io) '_platform_helper_io.dart'; // IO implementation (mobile/desktop)
+    if (dart.library.io) '../utils/_platform_helper_io.dart'; // IO implementation (mobile/desktop)
